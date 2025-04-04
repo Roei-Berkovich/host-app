69f72ee8e2b701304054ce777b27546f2d6c7f62
roei-berkovich <roei.berkovich@riverside.fm>
2025-04-05 01:02:09 +0300

    update shared dependencies to include react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
           from: 'vite',
         },
       },
-      shared: ['react', 'react-dom'],
+      shared: ['react', 'react-dom', 'react-router-dom'],
     }),
   ],
   build: {
